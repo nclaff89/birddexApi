01509408a21e3c2db0c9aabf61f7dd7a8f1e8053
Nick Claffey <nicholas.claffey@outlook.com>
2025-03-20 23:12:42 -0500

    git init

---
+++ src/main/java/com/nc/birddexApi/BirddexApiApplication.java
@@ -0,0 +1,13 @@
+package com.nc.birddexApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirddexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirddexApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nc/birddexApi/BirddexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nc.birddexApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirddexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
